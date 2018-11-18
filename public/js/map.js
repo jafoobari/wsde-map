fc48369072048a3cf231bd3e53f322c6d133db50
hyzypg <hyzypg@MacBook-Pro-3.local>
2018-11-18 15:32:36 -0600

    Change black (race) layer.

--- public/js/map.js
+++ public/js/map.js
@@ -5,53 +5,59 @@
 var darkmatter = L.tileLayer.provider('CartoDB.DarkMatterNoLabels'),
     positron = L.tileLayer.provider('CartoDB.PositronNoLabels'),
     income = L.tileLayer.provider('JusticeMap.income'),
-    black = L.tileLayer.provider('JusticeMap.black'),
-    nonwhite = L.tileLayer.provider('JusticeMap.nonWhite')
+    // black = L.tileLayer.provider('JusticeMap.black'),
+    // nonwhite = L.tileLayer.provider('JusticeMap.nonWhite'),
     lines = L.tileLayer.provider('Stamen.TonerLines'),
-    labels = L.tileLayer.provider('Stamen.TonerLabels');
-
+    labels = L.tileLayer.provider('Stamen.TonerLabels'),
+    blackBlockGroups = L.tileLayer('http://www.justicemap.org/tile/{size}/black/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://www.justicemap.org/terms.php">Justice Map</a>',
+    	  size: 'bg',
+        opacity: 0.5,
+    	  bounds: [[14, -180], [72, -56]]
+    });
 
 var mki = L.icon.mapkey({
-  icon:"adit",
-  color:"DarkRed",
-  background:false,
-  boxShadow:false,
-})
+    icon:"adit",
+    color:"DarkRed",
+    background:false,
+    boxShadow:false,
+});
 
 var markerStyle = L.geoJson(null, {
- onEachFeature: function(feature, layer) {
-     layer.bindPopup('<b>' + feature.properties.Title + '</b>' + '<br/>' + feature.properties.Location + '<br/>' + '<a href="' + feature.properties.Normalized_Website + '">' + feature.properties.Normalized_Website + '</a>');
- },
- pointToLayer: function (feature, latlng) {
-     return L.marker(latlng, {icon:mki});
-   }
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup('<b>' + feature.properties.Title + '</b>' + '<br/>' + feature.properties.Location + '<br/>' + '<a href="' + feature.properties.Normalized_Website + '">' + feature.properties.Normalized_Website + '</a>');
+    },
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {icon:mki});
+    }
 });
 
 var wsdeLayer = omnivore.csv('data/locations.csv', null, markerStyle)
-  .on("ready", function() {
-    var markers = L.markerClusterGroup();
-    markers.addLayer(wsdeLayer);
-    // markers.addTo(mymap);
-    var mymap = L.map('mapid', {
-        center: [35.59841484754639, -106.73698425292969],
-        zoom: 4,
-        layers: [positron, lines, labels, markers]
+    .on("ready", function() {
+        var markers = L.markerClusterGroup();
+        markers.addLayer(wsdeLayer);
+        // markers.addTo(mymap);
+        var mymap = L.map('mapid', {
+            center: [35.59841484754639, -106.73698425292969],
+            zoom: 4,
+            attributionControl: false, //Just leaving for development
+            layers: [positron, lines, labels, markers]
+        });
+
+        var overlayMaps = {
+            "Lines": lines,
+            "Labels": labels,
+            "Black (race)": blackBlockGroups,
+            // "POC": nonwhite,
+            "WSDEs": markers
+        };
+
+        var baseMaps = {
+            "Light": positron,
+            "Dark": darkmatter
+
+        };
+
+        L.control.layers(baseMaps, overlayMaps).addTo(mymap);
+
     });
-
-    var overlayMaps = {
-        "Lines": lines,
-        "Labels": labels,
-        "Black": black,
-        "POC": nonwhite,
-        "WSDEs": markers
-    };
-
-    var baseMaps = {
-        "Light": positron,
-        "Dark": darkmatter
-
-    };
-
-    L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-
-  });
