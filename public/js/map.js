c149de41cccca7155d928e70aae889f2973be0db
hyzypg <hyzypg@MacBook-Pro-3.local>
2018-11-20 20:59:39 -0600

    Dark base map, change cluster colors, add more granular racial layers

--- public/js/map.js
+++ public/js/map.js
@@ -2,24 +2,36 @@
 
 // var mymap = L.map('mapid').setView([35.59841484754639, -106.73698425292969], 3);
 
-var darkmatter = L.tileLayer.provider('CartoDB.DarkMatterNoLabels'),
-    positron = L.tileLayer.provider('CartoDB.PositronNoLabels'),
-    income = L.tileLayer.provider('JusticeMap.income'),
+var darkmatter = L.tileLayer.provider('CartoDB.DarkMatter'),
+    positron = L.tileLayer.provider('CartoDB.Positron'),
+    // income = L.tileLayer.provider('JusticeMap.income'),
     // black = L.tileLayer.provider('JusticeMap.black'),
     // nonwhite = L.tileLayer.provider('JusticeMap.nonWhite'),
-    lines = L.tileLayer.provider('Stamen.TonerLines'),
-    labels = L.tileLayer.provider('Stamen.TonerLabels'),
+    // lines = L.tileLayer.provider('Stamen.TonerLines'),
+    // labels = L.tileLayer.provider('Stamen.TonerLabels'),
     blackBlockGroups = L.tileLayer('http://www.justicemap.org/tile/{size}/black/{z}/{x}/{y}.png', {
         attribution: '<a href="http://www.justicemap.org/terms.php">Justice Map</a>',
     	  size: 'bg',
         opacity: 0.6,
     	  bounds: [[14, -180], [72, -56]]
+    }),
+    blackBlock = L.tileLayer('http://www.justicemap.org/tile/{size}/black/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://www.justicemap.org/terms.php">Justice Map</a>',
+    	  size: 'block',
+        opacity: 0.6,
+    	  bounds: [[14, -180], [72, -56]]
+    }),
+    blackDot = L.tileLayer('http://demographics.virginia.edu/DotMap/tiles4/{z}/{x}/{y}.png', {
+        attribution: '',
+        opacity: 0.7,
+    	  bounds: [[14, -180], [72, -56]]
     });
 
 var mki = L.icon.mapkey({
     icon:"adit",
-    color:"#562170",
-    background:false,
+    color:"#424242",
+    // hoverCSS: "background-color:#992b00",
+    background:"#FFFFFF",
     boxShadow:false,
 });
 
@@ -32,29 +44,41 @@ var markerStyle = L.geoJson(null, {
     }
 });
 
-var htmlLegend = L.control.htmllegend({
+var legendHtml = `<div style="font-size: 0.74em; width: auto; min-height: 112px; max-height: none; height: auto;"><div font-size:0.9em; width:90px; vertical-align:top;"></div>
+<div style="padding:2px;" ><span id="color_0" style="background:#ffffcc; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 0 - 1%</span></div><div style="padding:2px;"><span id="color_1" style="background:#ffeda0; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 1 - 2.5%</span></div><div style="padding:2px;"><span id="color_2" style="background:#fed976; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 2.5 - 5%</span></div><div style="padding:2px;"><span id="color_3" style="background:#feb24c; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 5 - 9%</span></div><div style="padding:2px;"><span id="color_4" style="background:#fd8d3c; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 9 - 16%</span></div><div style="padding:2px;"><span id="color_5" style="background:#fc4e2a; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 16 - 26%</span></div><div style="padding:2px;"><span id="color_6" style="background:#e31a1c; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 26 - 47%</span></div><div style="padding:2px;"><span id="color_7" style="background:#bd0026; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 47 - 79%</span></div><div style="padding:2px;"><span id="color_8" style="background:#800026; width:10px; height:10px;">
+   &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 79 - 100%</span></div>
+</div>`;
+
+var blackBlockGroupsLegend = L.control.htmllegend({
   position: 'bottomright',
   // collapsedOnInit: true,
   // defaultOpacity: 0.1,
   disableVisibilityControls: true,
   legends: [{
-          name: 'Black (race)',
-          layer: blackBlockGroups,
-          elements: [{
-              html: `<div style="font-size: 0.74em; width: auto; min-height: 112px; max-height: none; height: auto;"><div font-size:0.9em; width:90px; vertical-align:top;"></div>
-              <div style="padding:2px;" ><span id="color_0" style="background:#ffffcc; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 0 - 1%</span></div><div style="padding:2px;"><span id="color_1" style="background:#ffeda0; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 1 - 2.5%</span></div><div style="padding:2px;"><span id="color_2" style="background:#fed976; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 2.5 - 5%</span></div><div style="padding:2px;"><span id="color_3" style="background:#feb24c; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 5 - 9%</span></div><div style="padding:2px;"><span id="color_4" style="background:#fd8d3c; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 9 - 16%</span></div><div style="padding:2px;"><span id="color_5" style="background:#fc4e2a; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 16 - 26%</span></div><div style="padding:2px;"><span id="color_6" style="background:#e31a1c; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 26 - 47%</span></div><div style="padding:2px;"><span id="color_7" style="background:#bd0026; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 47 - 79%</span></div><div style="padding:2px;"><span id="color_8" style="background:#800026; width:10px; height:10px;">
-                 &nbsp;&nbsp;&nbsp;&nbsp;</span><span> 79 - 100%</span></div>
-              </div>`
-          }],
-      }]
+            name: 'Black (race) </br> pop. density',
+            layer: blackBlockGroups,
+            elements: [{ html: legendHtml }]
+    }]
+});
+
+var blackBlockLegend = L.control.htmllegend({
+  position: 'bottomright',
+  // collapsedOnInit: true,
+  // defaultOpacity: 0.1,
+  disableVisibilityControls: true,
+  legends: [{
+            name: 'Black (race) </br> pop. density',
+            layer: blackBlock,
+            elements: [{ html: legendHtml }]
+    }]
 });
 
 var wsdeLayer = omnivore.csv('data/locations.csv', null, markerStyle)
@@ -66,14 +90,17 @@ var wsdeLayer = omnivore.csv('data/locations.csv', null, markerStyle)
             center: [35.59841484754639, -106.73698425292969],
             zoom: 4,
             attributionControl: false, //Just leaving for development
-            layers: [positron, lines, labels, markers]
+            layers: [darkmatter, markers]
         });
 
         var overlayMaps = {
-            "Lines": lines,
-            "Labels": labels,
-            "Black (race) pop. density": blackBlockGroups,
+            // "Lines": lines,
+            // "Labels": labels,
+            "Black (race) block groups": blackBlockGroups,
+            "Black (race) blocks": blackBlock,
+            "ENHANCE!!": blackDot,
             // "POC": nonwhite,
+            // "income": income,
             "WSDEs": markers
         };
 
@@ -84,6 +111,6 @@ var wsdeLayer = omnivore.csv('data/locations.csv', null, markerStyle)
         };
 
         L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-        mymap.addControl(htmlLegend);
-
+        mymap.addControl(blackBlockGroupsLegend);
+        mymap.addControl(blackBlockLegend);
     });
